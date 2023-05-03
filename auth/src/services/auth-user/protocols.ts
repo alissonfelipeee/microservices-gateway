092da181a6bb8015f082e1cbec428134cd65ddc4
Alisson Felipe <alissonfelipenettt@gmail.com>
2023-05-03 10:49:17 -0300

    refactor[auth]: removed duplicate code

--- auth/src/services/auth-user/protocols.ts
+++ auth/src/services/auth-user/protocols.ts
@@ -1,12 +1,4 @@
-import {
-  AuthUserParams,
-  AuthUserResponse,
-} from "../../controllers/auth-user/protocols";
 import { Credential } from "../../models/Credential";
-
-export interface IAuthUserRepository {
-  auth(params: AuthUserParams): Promise<AuthUserResponse | null>;
-}
 
 export interface IGetUserCredentialsByEmailRepository {
   getUserCredentialsByEmail(email: string): Promise<Credential | null>;
