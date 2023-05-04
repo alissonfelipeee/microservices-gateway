8f9eb7cc1987b3162e6c854ba2a075a53749e9fb
Alisson Felipe <alissonfelipenettt@gmail.com>
2023-05-03 21:41:45 -0300

    refactor[auth]: removed unused code

--- auth/src/controllers/utils.ts
+++ auth/src/controllers/utils.ts
@@ -8,14 +8,6 @@ export const ok = <T>(message: string, body: any): HttpResponse<T> => ({
   },
 });
 
-export const created = <T>(body: any): HttpResponse<T> => ({
-  statusCode: HttpStatusCode.CREATED,
-  body: {
-    message: "User created successfully",
-    data: body,
-  },
-});
-
 export const badRequest = (message: string): HttpResponse<object> => {
   return {
     statusCode: HttpStatusCode.BAD_REQUEST,
@@ -29,16 +21,6 @@ export const badRequest = (message: string): HttpResponse<object> => {
 export const unauthorized = (message: string): HttpResponse<string> => {
   return {
     statusCode: HttpStatusCode.UNAUTHORIZED,
-    body: {
-      message,
-      data: null,
-    },
-  };
-};
-
-export const notFound = (message: string): HttpResponse<object> => {
-  return {
-    statusCode: HttpStatusCode.NOT_FOUND,
     body: {
       message,
       data: null,
